0efe0c8144df0d25439d0c41dfc1ecde8b642930
Bashar474 <9030281@student.zadkine.nl>
2026-04-10 14:41:14 +0200

    Update crud_klachten.php

--- src/crud_klachten.php
+++ src/crud_klachten.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
     <meta charset="UTF-8">
     <title>CRUD Klachten - TechZone</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-
     <?php
     // Auteur: Bashar
     // Functie: Klacht beheren
@@ -16,15 +13,10 @@
     include 'nav.php';
 
     echo "<h1>Klachten beheren</h1>";
-
-
     $result = getData("klachten");
 
     printTabel($result, 'klacht_id', 'klacht_update.php', 'klacht_delete.php', ['inloggen_id']);
 
-    include 'footer.php';
-    ?>
-
+    include 'footer.php'; ?>
 </body>
-
 </html>
