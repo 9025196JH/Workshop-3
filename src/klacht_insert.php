d518925fd16db94bcd0d57e0877c37edd6c15749
Bashar474 <9030281@student.zadkine.nl>
2026-04-10 13:41:11 +0200

    techzone-webshop

---
+++ src/klacht_insert.php
@@ -0,0 +1,57 @@
+<?php
+// Auteur: Bashar
+include_once 'functions.php';
+
+// Haal alle producten op voor de selectie-lijst
+$producten = getData('producten');
+
+if (isset($_POST['btn_ins'])) {
+    if (insertKlacht($_POST)) {
+        echo "<script>alert('Klacht is toegevoegd'); location.replace('crud_klachten.php');</script>";
+    } else {
+        echo "<script>alert('Fout bij toevoegen');</script>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Klacht Toevoegen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'nav.php'; ?>
+
+    <h1>Nieuwe Klacht Toevoegen</h1>
+
+    <form method="post">
+        <!-- Selecteer product -->
+        <label class="admin-label">Product:</label>
+        <select name="product_id" required>
+            <option value="">-- Kies een product --</option>
+            <?php foreach($producten as $p): ?>
+                <option value="<?php echo $p['product_id']; ?>">
+                    <?php echo $p['naam']; ?>
+                </option>
+            <?php endforeach; ?>
+        </select><br>
+
+        <label class="admin-label">Naam:</label>
+        <input type="text" name="naam" required><br>
+
+        <label class="admin-label">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label class="admin-label">Beschrijving:</label>
+        <textarea name="beschrijving" required></textarea><br>
+
+        <input type="submit" name="btn_ins" value="Insert">
+    </form>
+
+    <br>
+    <a href="crud_klachten.php">Terug naar overzicht</a>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
